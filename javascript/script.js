5cb79be2f27d508819d0439dfba6ace640d59cfb
Rahul <rahulchalat@gmail.com>
2022-05-10 17:50:09 +0530

    Updated Landing Page and Contact Us

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+/* const make = document.querySelector("#makeup");
+console.log(make);
+
+function changeColor() {} */
+
+const manicure = document.querySelector("#manicure");
+
+function changeThings() {
+  manicure.src = "images/makeup-white.png";
+  console.log("Hovered");
+}
